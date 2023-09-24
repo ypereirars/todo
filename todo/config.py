8993c5f9a6fe8978dc487be155a99e4651504482
Yuri Silveira <ypereirars@gmail.com>
2023-09-24 11:08:13 -0300

    feat: Configure the database file

--- todo/config.py
+++ todo/config.py
@@ -1,1 +1,63 @@
 """"Contains code to handle the application’s configuration file"""
+
+import configparser
+from pathlib import Path
+
+import typer
+
+from todo import ReturnCode, __app_name__
+
+CONFIG_DIR_PATH = Path(typer.get_app_dir(__app_name__))
+CONFIG_FILE_PATH = CONFIG_DIR_PATH / "config.ini"
+
+
+def init_app(db_path: str) -> ReturnCode:
+    """Initialize the application."""
+    config_code = _init_config_file()
+
+    if config_code != ReturnCode.SUCESS:
+        return config_code
+
+    database_code = _create_database(db_path)
+    if database_code != ReturnCode.SUCCESS:
+        return database_code
+
+    return ReturnCode.SUCCESS
+
+
+def _init_config_file() -> ReturnCode:
+    """Initialize the configuration file.
+
+    Returns:
+        ReturnCode: Return code.
+    """
+    try:
+        CONFIG_DIR_PATH.mkdir(exist_ok=True)
+    except OSError:
+        return ReturnCode.DIR_ERROR
+
+    try:
+        CONFIG_FILE_PATH.touch(exist_ok=True)
+    except OSError:
+        return ReturnCode.FILE_ERROR
+
+    return ReturnCode.SUCCESS
+
+
+def _create_database(db_path: str) -> ReturnCode:
+    """Create the database.
+
+    Args:
+        db_path (str): Path to the database.
+
+    Returns:
+        ReturnCode: Return code.
+    """
+    config_parser = configparser.ConfigParser()
+    config_parser["General"] = {"database": db_path}
+    try:
+        with CONFIG_FILE_PATH.open("w") as file:
+            config_parser.write(file)
+    except OSError:
+        return ReturnCode.DB_WRITE_ERROR
+    return ReturnCode.SUCCESS
--- todo/database.py
+++ todo/database.py
@@ -1,1 +1,40 @@
-"""Contains code to handle the application’s to-do database"""
+"""Contains code to handle the application's to-do database"""
+
+import configparser
+from pathlib import Path
+
+from todo import ReturnCode
+
+DEFAULT_DB_FILE_PATH = Path.home().joinpath(
+    "." + Path.home().stem + "_todo.json"
+)
+
+
+def get_database_path(config_file: Path) -> Path:
+    """Return the current path to the to-do database.
+
+    Args:
+        config_file (Path): Path to the configuration file.
+
+    Returns:
+        Path: Path to the to-do database.
+    """
+    config_parser = configparser.ConfigParser()
+    config_parser.read(config_file)
+    return Path(config_parser["General"]["database"])
+
+
+def init_database(db_path: Path) -> ReturnCode:
+    """Create the to-do database.
+
+    Args:
+        db_path (Path): Path to the to-do database.
+
+    Returns:
+        ReturnCode: Return code.    
+    """
+    try:
+        db_path.write_text("[]")  # Empty to-do list
+        return ReturnCode.SUCCESS
+    except OSError:
+        return ReturnCode.DB_WRITE_ERROR
