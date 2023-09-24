744bc2ab0e1ad6f175d67c88508d7d3766177ce8
Yuri Silveira <ypereirars@gmail.com>
2023-09-24 12:19:57 -0300

    feat: Add todo priority

--- todo/todo.py
+++ todo/todo.py
@@ -1,16 +1,31 @@
 """Provides code to connect the CLI with the to-do database"""
-from typing import Any, Dict, NamedTuple
+from enum import Enum
+from typing import NamedTuple
 
 from todo.database import DatabaseHandler
+
+
+class TodoPriority(Enum):
+    Low = 0
+    Medium = 1
+    High = 2
 
 
 class Todo:
     """Represents a to-do item"""
 
-    def __init__(self, id: int, title: str, description: str, completed: bool) -> None:
+    def __init__(
+        self,
+        id: int,
+        title: str,
+        description: str = "",
+        priority: TodoPriority = TodoPriority.Low,
+        completed: bool = False
+    ) -> None:
         self.id = id
         self.title = title
         self.description = description
+        self.priority = priority
         self.completed = completed
 
     def __repr__(self) -> str:
