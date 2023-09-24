a4d942f0c2aff593c0dee003becdc0abcf4d8184
Yuri Silveira <ypereirars@gmail.com>
2023-09-24 12:07:06 -0300

    feat: Create a todo model

--- todo/todo.py
+++ todo/todo.py
@@ -1,1 +1,22 @@
 """Provides code to connect the CLI with the to-do database"""
+from typing import Any, Dict, NamedTuple
+
+from todo.database import DatabaseHandler
+
+
+class Todo:
+    """Represents a to-do item"""
+
+    def __init__(self, id: int, title: str, description: str, completed: bool) -> None:
+        self.id = id
+        self.title = title
+        self.description = description
+        self.completed = completed
+
+    def __repr__(self) -> str:
+        return f"Todo: {self.id} {self.title} {self.completed}"
+
+
+class TodoModel(NamedTuple):
+    todo: Todo
+    error: int
