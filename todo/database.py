debe6c5458bc1aeac002fb78ae64d5ca251cb0bf
Yuri Silveira <ypereirars@gmail.com>
2023-09-24 12:07:23 -0300

    feat: Create todo database handler

--- todo/database.py
+++ todo/database.py
@@ -1,13 +1,46 @@
 """Contains code to handle the application's to-do database"""
 
 import configparser
+import json
 from pathlib import Path
+from typing import List, NamedTuple
 
 from todo import ReturnCode
+from todo.todo import Todo
 
 DEFAULT_DB_FILE_PATH = Path.home().joinpath(
     "." + Path.home().stem + "_todo.json"
 )
+
+
+class DBResponse(NamedTuple):
+    """Represents the response from the database."""
+
+    todo_list: List[Todo]
+    error: ReturnCode
+
+
+class DatabaseHandler:
+    def __init__(self, db_path: Path) -> None:
+        self._db_path = db_path
+
+    def read_todos(self) -> DBResponse:
+        try:
+            with self._db_path.open("r") as db:
+                try:
+                    return DBResponse(json.load(db), ReturnCode.SUCCESS)
+                except json.JSONDecodeError:  # Catch wrong JSON format
+                    return DBResponse([], ReturnCode.JSON_ERROR)
+        except OSError:  # Catch file IO problems
+            return DBResponse([], ReturnCode.DB_READ_ERROR)
+
+    def write_todos(self, todo_list: List[Todo]) -> DBResponse:
+        try:
+            with self._db_path.open("w") as db:
+                json.dump(todo_list, db, indent=4)
+            return DBResponse(todo_list, ReturnCode.SUCCESS)
+        except OSError:  # Catch file IO problems
+            return DBResponse(todo_list, ReturnCode.DB_WRITE_ERROR)
 
 
 def get_database_path(config_file: Path) -> Path:
