31997c157022c16a0f1cae35bdad293079d431df
Yuri Silveira <ypereirars@gmail.com>
2023-09-24 11:58:01 -0300

    feat: Add command to init database

--- todo/cli.py
+++ todo/cli.py
@@ -1,12 +1,44 @@
 """Provides the Command-Line Interface for the application"""
 
+from pathlib import Path
 from typing import Optional
 
-from typer import Exit, Option, Typer, echo
+from typer import Exit, Option, Typer, colors, echo, secho
 
-from todo import __app_name__, __version__
+from todo import ERRORS, ReturnCode, __app_name__, __version__, config, database
 
 app = Typer()
+
+
+@app.command()
+def init(
+    db_path: Optional[str] = Option(
+        str(database.DEFAULT_DB_FILE_PATH),
+        "--db-path",
+        "-db",
+        help="Path to the to-do database",
+        prompt="to-do database location?"
+    )
+) -> None:
+    """Initialize the application's configuration and database"""
+    app_return_code = config.init_app(db_path)
+
+    if app_return_code != ReturnCode.SUCCESS:
+        secho(
+            f'Creating config file failed with "{ERRORS[app_return_code]}"',
+            fg=colors.RED
+        )
+        raise Exit(code=app_return_code.value)
+
+    db_return_code = database.init_database(Path(db_path))
+    if db_return_code == ReturnCode.SUCCESS:
+        secho(f"The to-do database is {db_path}", fg=colors.GREEN)
+    else:
+        secho(
+            f'Creating database failed with "{ERRORS[db_return_code]}"',
+            fg=colors.RED,
+        )
+        raise Exit(1)
 
 
 def _version_callback(value: bool) -> None:
