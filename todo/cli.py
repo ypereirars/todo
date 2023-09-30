e831e7ddd6c04cb60d2233fa46fedfbf240c21d7
Yuri Silveira <ypereirars@gmail.com>
2023-09-30 11:32:43 -0300

    feat: Add functionality to add and list to-do's

--- todo/cli.py
+++ todo/cli.py
@@ -1,11 +1,12 @@
 """Provides the Command-Line Interface for the application"""
 
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional
 
-from typer import Exit, Option, Typer, colors, echo, secho
+from typer import Argument, Exit, Option, Typer, colors, echo, secho
 
 from todo import ERRORS, ReturnCode, __app_name__, __version__, config, database
+from todo.todo import TodoController
 
 app = Typer()
 
@@ -17,17 +18,14 @@ def init(
         "--db-path",
         "-db",
         help="Path to the to-do database",
-        prompt="to-do database location?"
+        prompt="to-do database location?",
     )
 ) -> None:
     """Initialize the application's configuration and database"""
     app_return_code = config.init_app(db_path)
 
     if app_return_code != ReturnCode.SUCCESS:
-        secho(
-            f'Creating config file failed with "{ERRORS[app_return_code]}"',
-            fg=colors.RED
-        )
+        secho(f'Creating config file failed with "{ERRORS[app_return_code]}"', fg=colors.RED)
         raise Exit(code=app_return_code.value)
 
     db_return_code = database.init_database(Path(db_path))
@@ -41,6 +39,109 @@ def init(
         raise Exit(1)
 
 
+@app.command()
+def add(
+    title: List[str] = Argument(..., help="Title of the to-do item"),
+    description: Optional[str] = Option(
+        [],
+        "--description",
+        "-d",
+        help="Description of the to-do item. Defaults to empty.",
+    ),
+    priority: Optional[int] = Option(
+        0,
+        "--priority",
+        "-p",
+        min=0,
+        max=3,
+        help="Priority of the to-do item. It may either be 0 (low), 1 (medium) or 2 (high). Defaults to 0.",
+    ),
+) -> None:
+    """Add a to-do item to the database"""
+    controller = get_todoer()
+
+    model = controller.add(title, description, priority)
+    if model.error == ReturnCode.SUCCESS:
+        secho(
+            f'to-do "{model.todo.title}" was added with {model.todo.priority.name.lower()} priority.',
+            fg=colors.GREEN,
+        )
+    else:
+        secho(f'Adding to-do item failed with "{ERRORS[model.error]}"', fg=colors.RED)
+        raise Exit(1)
+
+
+@app.command(name="list")
+def list_all(
+    completed: Optional[bool] = Option(
+        None,
+        "--completed",
+        "-c",
+        help="Filter completed to-do items",
+    ),
+    not_completed: Optional[bool] = Option(
+        None,
+        "--not-completed",
+        "-nc",
+        help="Filter not completed to-do items",
+    ),
+    priority: Optional[int] = Option(
+        None,
+        "--priority",
+        "-p",
+        min=0,
+        max=3,
+        help="Filter to-do items by priority",
+    ),
+) -> None:
+    """List to-do items"""
+    controller = get_todoer()
+
+    if completed is not None and not_completed is not None:
+        secho("You can't filter by both completed and not completed to-do items", fg=colors.RED)
+        raise Exit(1)
+
+    status = None
+    if completed is not None:
+        status = completed
+    elif not_completed is not None:
+        status = not not_completed
+
+    todo_list = controller.list(status, priority)
+
+    if len(todo_list) == 0:
+        secho("There are no tasks in the to-do list yet", fg=colors.RED)
+        raise Exit()
+
+    secho("\nto-do list:\n", fg=colors.BLUE, bold=True)
+    columns = (
+        f"{' '* 16} ID {' ' * 16} ",
+        "| Priority ",
+        "| Completed ",
+        "| Description  ",
+    )
+    headers = "".join(columns)
+    secho(headers, fg=colors.BLUE, bold=True)
+    secho("-" * len(headers), fg=colors.BLUE)
+
+    for todo in todo_list:
+        description = (" - " + todo.description) if todo.description else ""
+        description = description[: len(columns[3])] + "..." if len(description) > len(columns[3]) else description
+        secho(
+            f"{todo.id} "
+            f"|   {todo.priority.name}{(len(columns[1]) - len(str(todo.priority.name))-4) * ' '}"
+            f"|   {todo.completed}{(len(columns[2]) - len(str(todo.completed)) - 4) * ' '}"
+            f"| {todo.title}{description}{(len(columns[3]) - len(description) - 2) * ' '}",
+            fg=colors.BLUE,
+        )
+    secho("-" * len(headers) + "\n", fg=colors.BLUE)
+    # for todo in model.todo_list:
+    #     echo(f"{todo.id} {todo.title} {todo.priority.name.lower()} {todo.completed}")
+    # else:
+    #     secho(f'Listing to-do items failed with "{ERRORS[model.error]}"', fg=colors.RED)
+    #     raise Exit(1)
+
+
 def _version_callback(value: bool) -> None:
     """Print the version of the application"""
     if value:
@@ -48,13 +149,34 @@ def _version_callback(value: bool) -> None:
         raise Exit()
 
 
+def get_todoer() -> TodoController:
+    if config.CONFIG_FILE_PATH.exists():
+        db_path = database.get_database_path(config.CONFIG_FILE_PATH)
+    else:
+        secho(
+            'Config file not found. Please, run "rptodo init"',
+            fg=colors.RED,
+        )
+        raise Exit(1)
+    if db_path.exists():
+        return TodoController(db_path)
+    else:
+        secho(
+            'Database not found. Please, run "rptodo init"',
+            fg=colors.RED,
+        )
+        raise Exit(1)
+
+
 @app.callback()
 def main(
     version: Optional[bool] = Option(
-        None, "--version", "-v",
+        None,
+        "--version",
+        "-v",
         callback=_version_callback,
         is_eager=True,
-        help="Show the application's version and exit"
+        help="Show the application's version and exit",
     )
 ) -> None:
     """Manage your to-do list"""
--- todo/todo.py
+++ todo/todo.py
@@ -1,11 +1,13 @@
 """Provides code to connect the CLI with the to-do database"""
-from enum import Enum
-from typing import NamedTuple
+from enum import IntEnum
+from typing import List, NamedTuple, Optional
+from uuid import uuid1
 
+from todo import ReturnCode
 from todo.database import DatabaseHandler
 
 
-class TodoPriority(Enum):
+class TodoPriority(IntEnum):
     Low = 0
     Medium = 1
     High = 2
@@ -16,7 +18,7 @@ class Todo:
 
     def __init__(
         self,
-        id: int,
+        id: str,
         title: str,
         description: str = "",
         priority: TodoPriority = TodoPriority.Low,
@@ -25,7 +27,7 @@ class Todo:
         self.id = id
         self.title = title
         self.description = description
-        self.priority = priority
+        self.priority = TodoPriority(priority)
         self.completed = completed
 
     def __repr__(self) -> str:
@@ -41,5 +43,46 @@ class TodoController:
     def __init__(self, db_path: str) -> None:
         self._db_handler = DatabaseHandler(db_path)
 
-    def add(self, title: str, description: str = "", priority: TodoPriority = TodoPriority.Low) -> Todo:
-        return None
+    def add(
+        self, title: str | List[str], description: str | List[str] = "", priority: TodoPriority = TodoPriority.Low
+    ) -> TodoModel:
+        """Add a to-do item to the database
+
+        Args:
+            title (str): The title of the to-do item
+            description (str, optional): A description to the to-do item. Defaults to "".
+            priority (TodoPriority, optional): To-do priority. It may be low, medium or high. Defaults to Low.
+
+        Returns:
+            Todo: The to-do item added to the database
+        """
+        if isinstance(title, list):
+            title = " ".join(title)
+        if isinstance(description, list):
+            description = " ".join(description)
+
+        todo = Todo(str(uuid1()), title, description, priority, False)
+
+        read = self._db_handler.read_todos()
+
+        if read.error == ReturnCode.DB_READ_ERROR:
+            return TodoModel(todo, read.error)
+
+        read.todo_list.append(todo.__dict__)
+
+        write = self._db_handler.write_todos(read.todo_list)
+
+        return TodoModel(todo, write.error)
+
+    def list(self, completed: Optional[bool] = None, priority: Optional[int] = None) -> List[Todo]:
+        read = self._db_handler.read_todos()
+
+        todo_list = read.todo_list
+
+        todo_list = (
+            [todo for todo in todo_list if todo["completed"] == completed] if completed is not None else todo_list
+        )
+
+        todo_list = [todo for todo in todo_list if todo["priority"] == priority] if priority is not None else todo_list
+
+        return [Todo(**todo) for todo in todo_list]
